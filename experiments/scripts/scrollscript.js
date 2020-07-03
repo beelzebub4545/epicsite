c0fb89fda9a81428b874a3ecc17ea4df6269f171
Greyy <>
2020-07-03 15:56:09 +0200

    more or less done the scroll page is more or less done, i still have to figure a way out to put in text, but ill see how i can do that later

--- experiments/scripts/scrollscript.js
+++ experiments/scripts/scrollscript.js
@@ -2,15 +2,15 @@ $(document).ready(function(){
     var scroll_pos = 0;
     $(document).scroll(function() {
         scroll_pos = $(this).scrollTop();
-        if(scroll_pos > 1500) {
+        if(scroll_pos > 5*screen.height) {
             $("body").css('background-color', 'purple');
-        } else if(scroll_pos > 2400) {
+        } else if(scroll_pos > 4*screen.height) {
             $("body").css('background-color', 'blue');
-        } else if(scroll_pos > 1800) {
+        } else if(scroll_pos > 3*screen.height) {
             $("body").css('background-color', 'green');
-        } else if(scroll_pos > 1200) {
+        } else if(scroll_pos > 2*screen.height) {
             $("body").css('background-color', 'yellow');
-        } else if(scroll_pos > 600) {
+        } else if(scroll_pos > screen.height) {
             $("body").css('background-color', 'orange');
         } else {
             $("body").css('background-color', 'red');
