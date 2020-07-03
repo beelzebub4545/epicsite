ba5e6df48c76667c5d60bb4ca9300e316564eea3
Greyy <>
2020-07-03 17:07:16 +0200

    finished scrolling. finished the scrolling page, most importantly, text matching the colours.

--- experiments/scripts/scrollscript.js
+++ experiments/scripts/scrollscript.js
@@ -2,18 +2,31 @@ $(document).ready(function(){
     var scroll_pos = 0;
     $(document).scroll(function() {
         scroll_pos = $(this).scrollTop();
-        if(scroll_pos > 5*screen.height) {
-            $("body").css('background-color', 'purple');
-        } else if(scroll_pos > 4*screen.height) {
-            $("body").css('background-color', 'blue');
-        } else if(scroll_pos > 3*screen.height) {
-            $("body").css('background-color', 'green');
-        } else if(scroll_pos > 2*screen.height) {
-            $("body").css('background-color', 'yellow');
-        } else if(scroll_pos > screen.height) {
-            $("body").css('background-color', 'orange');
+        var height = $(window).height();
+        if(scroll_pos >= 2.5*height) {
+            $("body").css('background-color', 'rgb(58, 0, 58)');
+            $(".text-div").css('visibility', 'hidden');
+            $("#purple").css('visibility', 'visible');
+        } else if(scroll_pos >= 2*height) {
+            $("body").css('background-color', 'rgb(0, 0, 70');
+            $(".text-div").css('visibility', 'hidden');
+            $("#blue").css('visibility', 'visible');
+        } else if(scroll_pos >= 1.5*height) {
+            $("body").css('background-color', 'rgb(0, 58, 0)');
+            $(".text-div").css('visibility', 'hidden');
+            $("#green").css('visibility', 'visible');
+        } else if(scroll_pos >= height) {
+            $("body").css('background-color', 'rgb(122, 122, 0)');
+            $(".text-div").css('visibility', 'hidden');
+            $("#yellow").css('visibility', 'visible');
+        } else if(scroll_pos >= 0.5*height) {
+            $("body").css('background-color', ' rgb(124, 81, 0)');
+            $(".text-div").css('visibility', 'hidden');
+            $("#orange").css('visibility', 'visible');
         } else {
-            $("body").css('background-color', 'red');
+            $("body").css('background-color', 'rgb(102, 0, 0)');
+            $(".text-div").css('visibility', 'hidden');
+            $("#red").css('visibility', 'visible');
         }
     });
 });
